ff4ea90410a084446d889af7e9330d27a8324612
sceiler <yimin.yang0+github@gmail.com>
2024-10-16 17:11:07 +0200

    Update page.tsx

--- app/[code]/page.tsx
+++ app/[code]/page.tsx
@@ -61,7 +61,7 @@ export default async function Example({
                 type="submit"
                 className="mt-8 flex w-full items-center justify-center rounded-md border border-transparent bg-slate-600 px-8 py-3 text-base font-medium text-white hover:bg-slate-700 focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2"
               >
-                Add to cart
+                Add to cart12345
               </button>
             </form>
 
